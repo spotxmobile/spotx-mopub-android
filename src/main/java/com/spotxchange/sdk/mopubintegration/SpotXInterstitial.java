f17dbac63c76e1ecb9c3fcb3dafae7ca273ae944
Patrick <pmitchell@spotxchange.com>
2015-05-19 15:16:23 -0600

    CustomEventInterstitial stub
    Stub and slight build change to make Android Studio happy
    
    JIRA: YWS-2405

--- src/main/java/com/spotxchange/sdk/mopubintegration/SpotXInterstitial.java
+++ src/main/java/com/spotxchange/sdk/mopubintegration/SpotXInterstitial.java
@@ -1,4 +1,25 @@
 package com.spotxchange.sdk.mopubintegration;
 
+import android.content.Context;
+
+import com.mopub.mobileads.CustomEventInterstitial;
+
+import java.util.Map;
+
+
 public class SpotXInterstitial extends CustomEventInterstitial {
+    @Override
+    protected void loadInterstitial(Context context, CustomEventInterstitialListener customEventInterstitialListener, Map<String, Object> stringObjectMap, Map<String, String> stringStringMap) {
+
+    }
+
+    @Override
+    protected void showInterstitial() {
+
+    }
+
+    @Override
+    protected void onInvalidate() {
+
+    }
 }
