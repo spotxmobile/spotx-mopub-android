db338683656a5283dab89ba4a86c149a732b296f
Zachary Miller <zmiller@spotxchange.com>
2016-01-12 12:32:00 -0700

    Added SpotxRewardedVideo class and broke out some common methods into a new Common class

--- spotx-mopub-android/src/main/java/com/spotxchange/sdk/mopubintegration/SpotXInterstitial.java
+++ spotx-mopub-android/src/main/java/com/spotxchange/sdk/mopubintegration/SpotXInterstitial.java
@@ -19,15 +19,6 @@ import com.spotxchange.sdk.android.SpotxAdView;
 
 
 public class SpotXInterstitial extends CustomEventInterstitial {
-    public static final String CHANNEL_ID_KEY           = "channel_id";
-    public static final String IAB_CATEGORY_KEY         = "iab_category";
-    public static final String APP_STORE_URL_KEY        = "appstore_url";
-    public static final String PLAY_STORE_URL_KEY       = "playstore_url";
-    public static final String APP_DOMAIN_KEY           = "app_domain";
-    public static final String PREFETCH_KEY             = "prefetch";
-    public static final String AUTO_INIT_KEY            = "auto_init";
-    public static final String IN_APP_BROWSER_KEY       = "in_app_browser";
-    public static final String SECURE_CONNECTION_KEY    = "use_https";
 
     private SpotxAdView _adView;
 
@@ -98,7 +89,7 @@ public class SpotXInterstitial extends CustomEventInterstitial {
         )
     {
         _customEventInterstitialListener = customEventInterstitialListener;
-        SpotxAdSettings adSettings = constructAdSettings(localExtras, serverExtras);
+        SpotxAdSettings adSettings = Common.constructAdSettings(localExtras, serverExtras);
         _adView = new SpotxAdView(context, adSettings);
         _adView.setVisibility(View.INVISIBLE);
         _adView.setAdListener(_spotxAdListener);
@@ -115,58 +106,4 @@ public class SpotXInterstitial extends CustomEventInterstitial {
     protected void onInvalidate() {
         _adView = null;
     }
-
-    public static SpotxAdSettings constructAdSettings(Map<String,Object> localSettings, Map<String, String> defaultSettings){
-        Map<String,String> settings = defaultSettings;
-        settings.putAll(
-            convertStringObjectMapToStringStringMap(localSettings)
-            );
-
-        String channel = (settings.containsKey(CHANNEL_ID_KEY)) ? settings.get(CHANNEL_ID_KEY) : defaultSettings.get(CHANNEL_ID_KEY);
-        String appDomain = (settings.containsKey(APP_DOMAIN_KEY)) ? settings.get(APP_DOMAIN_KEY) : defaultSettings.get(APP_DOMAIN_KEY);
-
-        SpotxAdSettings adSettings = new SpotxAdSettings(Integer.valueOf(channel), appDomain, "interstitial");
-
-        if(settings.containsKey(APP_STORE_URL_KEY) || settings.containsKey(PLAY_STORE_URL_KEY)) {
-            String storeUrl = localSettings.containsKey(APP_STORE_URL_KEY) ? settings.get(APP_STORE_URL_KEY) : settings.get(PLAY_STORE_URL_KEY);
-            adSettings.setAppStoreUrl(storeUrl);
-        }
-        if(settings.containsKey(IAB_CATEGORY_KEY)) {
-            adSettings.setIabCategory(settings.get(IAB_CATEGORY_KEY));
-        }
-        if(settings.containsKey(AUTO_INIT_KEY)) {
-            boolean autoInit = Boolean.parseBoolean(settings.get(AUTO_INIT_KEY));
-            adSettings.setAutoInit(autoInit);
-        }
-        if(settings.containsKey(PREFETCH_KEY)){
-            boolean prefetch = Boolean.parseBoolean(settings.get(PREFETCH_KEY));
-            adSettings.setPrefetch(prefetch);
-        }
-        if(settings.containsKey(IN_APP_BROWSER_KEY)){
-            boolean shouldUse = Boolean.parseBoolean(settings.get(IN_APP_BROWSER_KEY));
-            adSettings.setShouldUseInternalBrowser(shouldUse);
-        }
-
-        // Optional configurations for SpotXView behavior.
-        if(settings.containsKey(SECURE_CONNECTION_KEY)){
-            boolean shouldUse = Boolean.parseBoolean(settings.get(SECURE_CONNECTION_KEY));
-            adSettings.setUseSecureConnection(shouldUse);
-        }
-
-        return adSettings;
-    }
-
-    public static Map<String,String> convertStringObjectMapToStringStringMap(Map<String, Object> map) {
-        Map<String, String> newMap = new HashMap<String,String>();
-        for (Map.Entry<String, Object> item : map.entrySet()) {
-            if (item.getValue() == null) {
-                newMap.put(item.getKey(), null);
-            }
-            else {
-                newMap.put(item.getKey(), item.getValue().toString());
-            }
-        }
-        return newMap;
-    }
-
 }
---
+++ spotx-mopub-android/src/main/java/com/spotxchange/sdk/mopubintegration/SpotxRewardedVideo.java
@@ -0,0 +1,249 @@
+package com.spotxchange.sdk.mopubintegration;
+
+import com.mopub.mobileads.CustomEventRewardedVideo;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.mopub.common.DataKeys;
+import com.mopub.common.LifecycleListener;
+import com.mopub.common.BaseLifecycleListener;
+import com.mopub.common.MediationSettings;
+import com.mopub.common.MoPubReward;
+import com.mopub.mobileads.MoPubErrorCode;
+import com.mopub.mobileads.MoPubRewardedVideoManager;
+
+import java.util.*;
+
+import com.spotxchange.sdk.android.SpotxAdListener;
+import com.spotxchange.sdk.android.SpotxAdSettings;
+import com.spotxchange.sdk.android.SpotxAdView;
+
+/**
+ *
+ */
+public class SpotxRewardedVideo extends CustomEventRewardedVideo
+{
+
+    private static final String DEFAULT_SPOTX_APP_ID = "default_app_id";
+
+    /*
+     * Constants inded for internal MoPub use. Do not modify.
+     */
+    public static final String APP_ID_KEY = "appId";
+    public static final String SPOTX_AD_NETWORK_CONSTANT = "spotx";
+
+    // This has to be reinitiialized every time the CE loads to avoid conflict with the interstitials.
+    private SpotxAdView sSpotxAdView;
+    private  SpotxRewardedVideoListener sSpotxListener;
+    private boolean sInitialized;
+    private String mAdUnitId;
+    private boolean bIsAdAvailable;
+
+    private static final LifecycleListener sLifecycleListener = new BaseLifecycleListener(){
+        @Override
+        public void onPause(@NonNull final Activity activity){
+            super.onResume(activity);
+        }
+
+        @Override
+        public void onResume(@NonNull final Activity activity){
+            super.onResume(activity);
+        }
+    };
+
+    public SpotxRewardedVideo(){
+        sSpotxAdView = null;
+        bIsAdAvailable = false;
+        sSpotxListener = new SpotxRewardedVideoListener();
+        sInitialized = false;
+        mAdUnitId = null;
+    }
+
+    @Nullable
+    @Override
+    protected CustomEventRewardedVideoListener getVideoListenerForSdk() {
+        return sSpotxListener;
+    }
+
+    /**
+     * Provides a {@link LifecycleListener} if the custom event's ad network wishes to be notified of
+     * activity lifecycle events in the application.
+     *
+     * @return a LifecycleListener. May be null.
+     */
+    @Nullable
+    // @VisibleForTesting
+    protected LifecycleListener getLifecycleListener(){
+        return sLifecycleListener;
+    }
+
+    /**
+     * Called by the {@link MoPubRewardedVideoManager} after loading the custom event.
+     * This should return the "ad unit id", "zone id" or similar identifier for the network.
+     * May be empty if the network does not have anything more specific than an application ID.
+     *
+     * @return the id string for this ad unit with the ad network.
+     */
+    @NonNull
+    protected String getAdNetworkId(){
+        return SPOTX_AD_NETWORK_CONSTANT;
+    };
+
+    /**
+     * Called to when the custom event is no longer used. Implementers should cancel any
+     * pending requests. The initialized SDK may be reused by another CustomEvent instance
+     * and should not be shut down or cleaned up.
+     */
+    protected void onInvalidate(){
+        bIsAdAvailable = false;
+    }
+
+    /**
+     * Sets up the 3rd party ads SDK if it needs configuration. Extenders should use this
+     * to do any static initialization the first time this method is run by any class instance.
+     * From then on, the SDK should be reused without initialization.
+     *
+     * @return true if the SDK performed initialization, false if the SDK was already initialized.
+     */
+    protected  boolean checkAndInitializeSdk(@NonNull Activity launcherActivity,
+                                             @NonNull Map<String, Object> localExtras,
+                                             @NonNull Map<String, String> serverExtras)
+            throws Exception{
+        synchronized (SpotxRewardedVideo.class){
+            if(!sInitialized){
+                SpotxAdSettings adSettings = Common.constructAdSettings(localExtras, serverExtras);
+                sSpotxAdView = new SpotxAdView(launcherActivity, adSettings);
+                sInitialized = true;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Runs the ad-loading logic for the 3rd party SDK. localExtras & serverExtras should together
+     * contain all the data needed to load an ad.
+     *
+     * Implementers should also use this method (or checkAndInitializeSdk)
+     * to register a listener for their SDK, wrap it in a
+     * {@link com.mopub.mobileads.CustomEventRewardedVideo.CustomEventRewardedVideoListener}
+     *
+     * This method should not call any {@link MoPubRewardedVideoManager} event methods directly
+     * (onAdLoadSuccess, etc). Instead the SDK delegate/listener should call these methods.
+     *
+     * @param activity the "main activity" of the app. Useful for initializing sdks.
+     * @param localExtras
+     * @param serverExtras
+     */
+    protected  void loadWithSdkInitialized(@NonNull Activity activity,
+                                           @NonNull Map<String, Object> localExtras,
+                                           @NonNull Map<String, String> serverExtras)
+            throws Exception{
+
+        SpotxAdSettings adSettings = Common.constructAdSettings(localExtras, serverExtras);
+        sSpotxAdView.setAdSettings(adSettings);
+        sSpotxAdView.setAdListener(sSpotxListener);
+        sSpotxAdView.setVisibility(View.INVISIBLE);
+        activity.addContentView(sSpotxAdView, new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        sSpotxAdView.init();
+        Object adUnitObject = localExtras.get(DataKeys.AD_UNIT_ID_KEY);
+        if(adUnitObject instanceof String){
+            this.mAdUnitId = (String) adUnitObject;
+        }
+    }
+
+    /**
+     * Implementers should query the 3rd party SDK for whether there is a video available for the
+     * 3rd party SDK & ID represented by the custom event.
+     *
+     * @return true iff a video is available to play.
+     */
+    @Override
+    protected boolean hasVideoAvailable(){
+        return bIsAdAvailable;
+    }
+
+    /**
+     * Implementers should now play the rewarded video for this custom event.
+     */
+    protected void showVideo(){
+        sSpotxAdView.setVisibility(View.VISIBLE);
+    }
+
+    private class SpotxRewardedVideoListener implements CustomEventRewardedVideoListener, SpotxAdListener {
+
+        @Override
+        public void adLoaded() {
+            MoPubRewardedVideoManager.onRewardedVideoLoadSuccess(
+                    SpotxRewardedVideo.class,
+                    SPOTX_AD_NETWORK_CONSTANT);
+            bIsAdAvailable = true;
+        }
+
+        @Override
+        public void adStarted() {
+            MoPubRewardedVideoManager.onRewardedVideoStarted(
+                    SpotxRewardedVideo.class,
+                    SPOTX_AD_NETWORK_CONSTANT);
+        }
+
+        @Override
+        public void adCompleted() {
+            sSpotxAdView.setVisibility(View.INVISIBLE);
+            MoPubRewardedVideoManager.onRewardedVideoCompleted(
+                    SpotxRewardedVideo.class,
+                    SPOTX_AD_NETWORK_CONSTANT,
+                    MoPubReward.success(MoPubReward.NO_REWARD_LABEL, MoPubReward.NO_REWARD_AMOUNT));
+        }
+
+        @Override
+        public void adError() {
+            MoPubRewardedVideoManager.onRewardedVideoLoadFailure(
+                    SpotxRewardedVideo.class,
+                    SPOTX_AD_NETWORK_CONSTANT,
+                    MoPubErrorCode.UNSPECIFIED);
+        }
+
+        @Override
+        public void adExpired() {
+            MoPubRewardedVideoManager.onRewardedVideoLoadFailure(
+                    SpotxRewardedVideo.class,
+                    SPOTX_AD_NETWORK_CONSTANT,
+                    MoPubErrorCode.UNSPECIFIED);
+        }
+
+        @Override
+        public void adClicked() {
+            MoPubRewardedVideoManager.onRewardedVideoClicked(
+                    SpotxRewardedVideo.class,
+                    SPOTX_AD_NETWORK_CONSTANT);
+        }
+    }
+
+    public static class SpotxMediationSettings implements MediationSettings
+    {
+        @Nullable private final String channel_id;
+
+        public static class Builder {
+            @Nullable private String channel_id;
+
+            public Builder withChannelId(@NonNull final String channel_id) {
+                this.channel_id = channel_id;
+                return this;
+            }
+
+            public SpotxMediationSettings build() {
+                return new SpotxMediationSettings(this);
+            }
+        }
+
+        private SpotxMediationSettings(@NonNull final Builder builder) {
+            this.channel_id = builder.channel_id;
+        }
+    }
+}
